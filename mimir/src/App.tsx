6bcd4eb4772a4622ecdec12b6ad02528fffaa63b
Gautham Arumugam <gaut1993@gmail.com>
2022-09-18 12:29:10 +0200

    more functions

--- mimir/src/App.tsx
+++ mimir/src/App.tsx
@@ -1,16 +1,20 @@
-import React, { useReducer } from "react";
+import React from "react";
 import "./App.css";
 import { NavBarComponent } from "./components/NavBarComponent";
 import { GamePageComponent } from "./components/gamepage/GamePageComponent";
-import {gameReducer, initialGame} from "./store/GameReducer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
-    <div className="App">
-      <NavBarComponent ></NavBarComponent>
-      <GamePageComponent />
-    </div>
+      <Routes>
+        <Route>
+          <div className="App">
+            <NavBarComponent></NavBarComponent>
+            <GamePageComponent />
+          </div>
+        </Route>
+        <Route></Route>
+      </Routes>
   );
 }
 
